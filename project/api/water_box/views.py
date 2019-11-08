edd5b26b75142277aaaa30c34a59c0912a64eb96
phe0 <pheofo@gmail.com>
2019-11-08 15:06:24 -0300

    create post waterbox route

--- project/api/water_box/views.py
+++ project/api/water_box/views.py
@@ -1,3 +1,38 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify
+from project.api.water_box.model import WaterBox
+
+from sqlalchemy import exc
+from project import db
 
 water_box_blueprint = Blueprint('/water-box', __name__)
+
+@water_box_blueprint.route('/water_box', methods=['POST'])
+def create_water_box():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({
+            'error': 'Water box can not be created'
+        }), 400
+
+    material        = data.get('material')
+    volume          = data.get('volume')
+    position        = data.get('position')
+    pipe_size       = data.get('pipe_size')
+
+    try:
+        water_box = WaterBox(material, volume, position, pipe_size)
+        db.session.add(water_box)
+        db.session.commit()
+
+        return jsonify({
+            'status': 'success',
+            'data': {
+                'message': 'Water box was created'
+            }
+        }), 200
+    except exc.IntegrityError:
+        db.session.rollback()
+        return jsonify({
+            'error': 'Water box can not be created'
+        }), 400
